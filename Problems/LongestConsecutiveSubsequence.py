8d68c1bc6432efd04850ceea47587b6d4f25702f
Swapnil (Personal) <trambake.swapnil@gmail.com>
2021-07-30 19:52:21 +0530

    Added Union-Find Algorithm

---
+++ Problems/LongestConsecutiveSubsequence.py
@@ -0,0 +1,69 @@
+#
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+# You must write an algorithm that runs in O(n) time.
+#
+# @author: Swapnil Trambake, trambake.swapnil@gmail.com
+#
+#   Example 1:
+#
+#   Input: nums = [100,4,200,1,3,2]
+#   Output: 4
+#   Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+#   Example 2:
+#
+#   Input: nums = [0,3,7,2,5,8,4,6,0,1]
+#   Output: 9
+# 
+#
+#   Constraints:
+#
+#   0 <= nums.length <= 105
+#   -109 <= nums[i] <= 109
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import List
+
+from data_structures.UnionFind.UnionFind import UnionFind
+
+
+class UsingUnionFind(object):
+    def __init__(self, input : List[int]) -> None:
+        super().__init__()
+        self.__list = input
+
+        self.__unionFind = UnionFind()
+
+    def calculateLongestConsecutiveSubsequence(self):
+        for item in self.__list:
+            # Check if item is present in any set
+            root = self.__unionFind.find(item)
+            
+            if not root:
+                # Item doesn't exists in any set
+                self.__unionFind.makeSet(item)
+            
+            # Let's find next or previous consecutive number exists in set
+            nextRoot = self.__unionFind.find(item + 1)
+            prevRoot = self.__unionFind.find(item - 1)
+            if nextRoot:
+                self.__unionFind.unify(item, item+1)
+            if prevRoot:
+                self.__unionFind.unify(item, item-1) 
+
+        counts = self.__unionFind.getSetItemsCount()
+        max = 0
+        for count in counts:
+            if max < count:
+                max = count
+
+        print ('Longest consecutive subsquence using Union-Find is {}'.format(max))
+                
+
+
+if __name__ == "__main__":
+    input = [100,4,200,1,3,2]
+    print ('Input: {}'.format(input))
+    usingUnionFind = UsingUnionFind(input)
+    usingUnionFind.calculateLongestConsecutiveSubsequence()
---
+++ data_structures/UnionFind/UnionFind.py
@@ -0,0 +1,79 @@
+#
+#   This is implementation of Union-Find data structure, also known as Disjoint Set or Merge-Find set
+#   @author Swapnil Trambake, trambake.swapnil@gmail.com
+#
+
+import pprint
+
+class UnionFind(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # key - data, value - parent
+        self.__items = dict()
+
+
+    def find(self, item):
+        """
+        Find the item is present in any set, if present returns the root else None
+        """
+        root = None
+        if item in self.__items.keys():
+            root = item
+            
+            while self.__items[root] != root:
+                root = self.__items[root]
+
+        return root
+
+
+    def makeSet(self, item):
+        """
+        Creates a new set with the item
+        """
+        if not self.find(item):
+            # add the item as an independant set and make root as itself
+            self.__items[item] = item
+
+
+    def unify(self, firstItem, secondItem):
+        """
+        Merges two different set
+        """
+        root1 = self.find(firstItem)
+        root2 = self.find(secondItem)
+
+        if root1 > root2:
+            self.__items[root2] = root1
+        else:
+            self.__items[root1] = root2
+
+
+    def getSetItemsCount(self):
+        """
+        Returns list of set item count 
+        """
+        sets = self.__getSets()
+        return [len(arr) for arr in sets.values()]
+
+
+    def printSets(self):
+        """
+        Prints all sets 
+        """
+        sets = self.__getSets()
+        pprint.pprint(sets)
+        
+
+    def __getSets(self):
+        """
+        Get all sets 
+        """
+        sets = dict()
+        for item, root in self.__items.items():
+            root = self.find(item)
+
+            if root not in sets.keys():
+                sets[root] = []
+            sets[root].append(item)
+        return sets
