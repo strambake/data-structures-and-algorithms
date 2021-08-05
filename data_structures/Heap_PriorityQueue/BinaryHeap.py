b5c8040235ea69e58087058442b6f42fdce93da9
strambake <86815109+strambake@users.noreply.github.com>
2021-08-05 12:55:18 +0530

    Added PyLint Configration (#1)
    * Added PyLint Configration

--- data_structures/Heap_PriorityQueue/BinaryHeap.py
+++ data_structures/Heap_PriorityQueue/BinaryHeap.py
@@ -1,9 +1,12 @@
-#
-# A min priority queue implementation using a binary heap.
-# @author Swapnil Trambake, trambake.swapnil@gmail.com
-#
+"""
+A min priority queue implementation using a binary heap.
+@author Swapnil Trambake, trambake.swapnil@gmail.com
+"""
 
-class BinaryHeap(object):
+class BinaryHeap():
+    """
+    Class implements binary heap using array
+    """
     def __init__(self) -> None:
         super().__init__()
 
@@ -11,16 +14,25 @@ class BinaryHeap(object):
 
 
     def print(self):
+        """
+        Prints heap on console
+        """
         print('The heap is: {}'.format(self.__heap))
 
 
     def add(self, item : int):
+        """
+        Add element into binary heap
+        """
         print('Adding {}'.format(item))
         self.__heap.append(item)
         self.__swim(len(self.__heap) - 1)
 
 
     def poll(self):
+        """
+        Poll the heap, which gets high priority element
+        """
         value = self.__heap[0]
         self.__remove(0)
         print('Polled {}'.format(value))
@@ -28,52 +40,54 @@ class BinaryHeap(object):
 
 
     def remove(self, value):
+        """
+        Removes specific element from heap
+        """
         index = self.__find(value)
         if index:
             self.__remove(index)
-    
 
-    def __swim(self, index):
-        """
-        Perform bottom up swim o(log(n)) 
-        """
-        while True:
-            parent = int((index - 1) / 2)
-            if parent == index:
-                break
-            elif self.__heap[parent] >= self.__heap[index]:
-                self.__swap(parent, index)
-                index = parent
-            else:
-                break
-            
 
     def __remove(self, index):
         print('Removing {} at index {}'.format(self.__heap[index], index))
-        lastIndex = len(self.__heap) - 1
-        if index != lastIndex:
-            self.__swap(index, lastIndex)
-            del self.__heap[lastIndex]
+        last_index = len(self.__heap) - 1
+        if index != last_index:
+            self.__swap(index, last_index)
+            del self.__heap[last_index]
 
-            parentIndex = int((index - 1) / 2)
-            if parentIndex <= 0 or self.__heap[parentIndex] < self.__heap[index]:
+            parent_index = int((index - 1) / 2)
+            if parent_index <= 0 or self.__heap[parent_index] < self.__heap[index]:
                 self.__sink(index)
             else:
                 self.__swim(index)
 
 
     def __find(self, value):
-        print('Findi
-        ng {} in heap'.format(value))
+        print('Finding {} in heap'.format(value))
         index = None
-        
-        for i in range(len(self.__heap)):
-            if value == self.__heap[i]:
+
+        for i, val in enumerate(self.__heap):
+            if value == val:
                 index = i
                 break
 
         print ('{} value {} in heap'.format('Found' if index else 'Not found', value))
         return index
+
+
+    def __swim(self, index):
+        """
+        Perform bottom up swim o(log(n))
+        """
+        while True:
+            parent = int((index - 1) / 2)
+            if parent != index and \
+                self.__heap[parent] >= self.__heap[index]:
+                self.__swap(parent, index)
+                index = parent
+            else:
+                break
+
 
 
     def __sink(self, index):
@@ -83,11 +97,14 @@ class BinaryHeap(object):
         while index < len(self.__heap):
             leftChildIndex = 2 * index + 1
             rightChildIndex = 2 * index + 2
-            
-            if leftChildIndex < len(self.__heap) or rightChildIndex < len(self.__heap):                
+
+            if leftChildIndex < len(self.__heap) or rightChildIndex < len(self.__heap):
                 if rightChildIndex < len(self.__heap):
-                    if self.__heap[index] > self.__heap[leftChildIndex] and self.__heap[index] > self.__heap[rightChildIndex]:
-                        indexToReplace = leftChildIndex if self.__heap[leftChildIndex] <= self.__heap[rightChildIndex] else rightChildIndex
+                    if self.__heap[index] > self.__heap[leftChildIndex] and \
+                        self.__heap[index] > self.__heap[rightChildIndex]:
+                        indexToReplace = leftChildIndex \
+                            if self.__heap[leftChildIndex] <= self.__heap[rightChildIndex] \
+                            else rightChildIndex
                     elif self.__heap[index] > self.__heap[leftChildIndex]:
                         indexToReplace = leftChildIndex
                     else:
