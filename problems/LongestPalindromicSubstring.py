34d9cf58b3c334ee6f2d0f200646a93582a0a216
strambake <trambake.swapnil@gmail.com>
2021-08-13 14:28:47 +0530

    Longest palindromic substring solution with o(n^2)

--- problems/LongestPalindromicSubstring.py
+++ problems/LongestPalindromicSubstring.py
@@ -24,23 +24,31 @@ from utility.LogHelper import logger
 from unittest import TestCase
 
 
-class NaiveSolution():
+class On2Solution1():
     def longestPalindrome(self, s: str) -> str:
-        length = len(s)
         result = ''
-        for index1 in range(0, length):
-            for index2 in range(index1, length):
-                subString = s[index1:(index2+1)] if index1 != index2 else s[index1]
-                
-                if self.__isPalindrome(subString):
-                    if len(subString) > len(result):
-                        result = subString
-                    break
+        if s:
+            length = len(s)
+            for index1 in range(0, length):
+                for index2 in range(index1, length):
+                    subString = s[index1:index2+1] if index1 != index2 else s[index1]
+                    
+                    if self.__isPalindrome(subString):
+                        if len(subString) > len(result):
+                            result = subString
 
         return result
 
 
     def __isPalindrome(self, string: str) -> bool:
+        """Worst solution takes o(n^3)
+
+        Args:
+            string (str): [description]
+
+        Returns:
+            bool: [description]
+        """
         logger.debug('Checking substring {} for palindrome...'.format(string))
         palindrome = False
         if string:
@@ -60,29 +68,81 @@ class NaiveSolution():
         return palindrome
 
 
-class TestNaiveSolution(TestCase):
-    def testLongestPalindromicSubstring(self):
-        logger.debug('Hey hey ...')
-        naiveSolution = NaiveSolution()
-        self.assertEqual('bab', naiveSolution.longestPalindrome('babad'))
-        self.assertEqual('bb', naiveSolution.longestPalindrome('cbbd'))
-        self.assertEqual('a', naiveSolution.longestPalindrome('a'))
-        self.assertEqual('a', naiveSolution.longestPalindrome('ac'))
-        self.assertEqual('', naiveSolution.longestPalindrome(''))
-        self.assertEqual('bb', naiveSolution.longestPalindrome('bb'))
-        # self.assertEqual('', naiveSolution.longestPalindrome('civilwartestingwhetherthatnaptionoranynartionsoc'\
-        #     'onceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefieml'\
-        #         'doftzhatwarWehavecometodedicpateaportionofthatfieldasafinal'\
-        #             'restingplaceforthosewhoheregavetheirlivesthatthatnationm'\
-        #                 'ightliveItisaltogetherfangandproperthatweshoulddothisBu'\
-        #                     'tinalargersensewecannotdedicatewecannotconsecratewecan'\
-        #                         'nothallowthisgroundThebravelmenlivinganddeadwhostruggled'\
-        #                             'herehaveconsecrateditfaraboveourpoorponwertoaddordetractTghew'\
-        #                                 'orldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwh'\
-        #                                     'attheydidhereItisforusthelivingrathertobededicatedheretotheulnfinished'\
-        #                                         'workwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobehere'\
-        #                                             'dedicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetake'\
-        #                                                 'increaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthat'\
-        #                                                     'weherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsder'\
-        #                                                         'Godshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeople'\
-        #                                                             'shallnotperishfromtheearth'))
+class On2Solution2():
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        if s:
+            start = end = 0            
+
+            for i in range(len(s)):
+                length1 = self.__getLongestPalindromeFromMidIndex(s, i, i)
+                length2 = self.__getLongestPalindromeFromMidIndex(s, i, i+1)
+                length = max(length1, length2)
+                if length > (end - start + 1):
+                    start = i - int((length-1)/2)
+                    end = i + int(length/2)
+            res = s[start:(end+1)]
+
+        return res
+
+
+    def __getLongestPalindromeFromMidIndex(self, s: str, left: int, right: int) -> int:
+        if s and left <= right:
+            while left >=0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            res = right - left - 1
+        else:
+            res = 0
+
+        return res
+
+
+
+class TestLongestPalindromicSubstring(TestCase):
+    def setUp(self) -> None:
+        self.testData = {   'babad' :   'bab',
+                            'cbbd'  :   'bb',
+                            'a'     :   'a',
+                            'ac'    :   'a',
+                            ''      :   '',
+                            'bb'    :   'bb'    }
+        return super().setUp()
+
+
+
+    def testOn2Solution1(self):
+        logger.debug('\nTesting with naive solution having o(n^2)...')
+        on2Sol = On2Solution1()
+
+        for testString, result in self.testData.items(): 
+            self.assertEqual(result, on2Sol.longestPalindrome(testString), 'Getting longest palindrome of \'{}\''.format(testString))
+
+
+    def testOn2Solution2(self):
+        logger.debug('\nTesting with better solution having o(n^2)...')
+        on2Sol = On2Solution2()
+
+        for testString, result in self.testData.items():
+            self.assertEqual(result, on2Sol.longestPalindrome(testString), 'Getting longest palindrome of \'{}\''.format(testString))
+
+
+    def testOn2SolutionLongInput(self):
+        on2Sol = On2Solution2()
+        input = """civilwartestingwhetherthatnaptionoranynartionsoc
+                onceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefieml
+                doftzhatwarWehavecometodedicpateaportionofthatfieldasafinal
+                restingplaceforthosewhoheregavetheirlivesthatthatnationm
+                ightliveItisaltogetherfangandproperthatweshoulddothisBu
+                tinalargersensewecannotdedicatewecannotconsecratewecan
+                nothallowthisgroundThebravelmenlivinganddeadwhostruggled
+                herehaveconsecrateditfaraboveourpoorponwertoaddordetractTghew
+                orldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwh
+                attheydidhereItisforusthelivingrathertobededicatedheretotheulnfinished
+                workwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobehere
+                dedicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetake
+                increaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthat
+                weherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsder
+                Godshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeople
+                shallnotperishfromtheearth"""
+        self.assertEqual('', on2Sol.longestPalindrome(input).strip())
