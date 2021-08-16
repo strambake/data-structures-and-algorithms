18cce1e2115bb261143da8ab8b4e687d0dd3695d
strambake <86815109+strambake@users.noreply.github.com>
2021-08-16 18:41:59 +0530

    Tile a board of n length bottom up (#29)
    * Tile a board of n length bottom up
    
    * Tile a board of length n with bottom up approach
    
    * test
    
    * pylint fix

--- problems/dynamic_programming/tilling_problems/TileABoardOfNLength.py
+++ problems/dynamic_programming/tilling_problems/TileABoardOfNLength.py
@@ -13,8 +13,8 @@ class Solution():
 
     def tileBoardTopDown(self, boardLength: int, tile1Length: int, tile2Length: int):
         """
-        Using top-down approach, 
-        1. Add a either of tile on a board 
+        Using top-down approach,
+        1. Add a either of tile on a board
         2. Recusively add all possible tile and find way to tile a board
         i.e Let f(n) is a function to calulate the number of ways to tile a board
         then f(n) = f(n-tile1) + f(n-tile2)
@@ -22,19 +22,19 @@ class Solution():
                             f(5)
                 f(4)                    f(3)
             f(3)    f(2)            f(2)    f(1)
-        f(2)            f(0)     f(1)           f(0)   
+        f(2)            f(0)     f(1)           f(0)
         """
         self.__tile1Length = tile1Length
-        self.__tile2Length = tile2Length       
-        self.__dp = [None] * (boardLength+1) 
+        self.__tile2Length = tile2Length
+        self.__dp = [None] * (boardLength+1)
         return self.__tileBoardTopDown(boardLength)
 
-    
+
     def __tileBoardTopDown(self, boardLength: int):
         # If board length is in negative means last added tile doesn't complete the board
-        # Return 0 i.e. no possible tilling 
+        # Return 0 i.e. no possible tilling
         if boardLength < 0:
-            # If board 
+            # If board
             return 0
 
         # If board length is 0 means after adding last tile the board is complete
@@ -48,12 +48,31 @@ class Solution():
             return self.__dp[boardLength]
 
         # Find a possible ways for all possible combination
-        self.__dp[boardLength] = self.__tileBoardTopDown(boardLength-self.__tile1Length) + self.__tileBoardTopDown(boardLength-self.__tile2Length)
+        self.__dp[boardLength] = self.__tileBoardTopDown(boardLength-self.__tile1Length) + \
+             self.__tileBoardTopDown(boardLength-self.__tile2Length)
         return self.__dp[boardLength]
-    
+
+
+    @staticmethod
+    def tileBoardBottomUp(boardLength: int):
+        if boardLength in (0, 1):
+            return 1
+
+        dp = [0] * (boardLength+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, boardLength+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[boardLength]
 
 
 class TestCalculateTotalWaysToCompleteABoard(TestCase):
-    def test(self):
+    def testTopDown(self):
         sol = Solution()
         self.assertEqual(8, sol.tileBoardTopDown(5, 1, 2))
+
+
+    def testBottomUp(self):
+        sol = Solution()
+        self.assertEqual(8, sol.tileBoardBottomUp(5))
