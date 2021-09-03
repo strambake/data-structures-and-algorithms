f1c24feaf20910f1a924b49f8be07f979d7b6275
strambake <86815109+strambake@users.noreply.github.com>
2021-09-03 13:28:58 +0530

    Merge two sorted array (#31)
    * Merge two sorted array
    
    * pylint fix
    
    * Pylint fix

---
+++ problems/LinkedLists/__init__.py
@@ -0,0 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'utility'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'data_structures'))
