9fc6eefa31454e9b8e8223a6c3d5eef7db42509a
strambake <trambake.swapnil@gmail.com>
2021-09-03 13:24:00 +0530

    pylint fix

--- problems/LinkedLists/MergeTwoSortedLists.py
+++ problems/LinkedLists/MergeTwoSortedLists.py
@@ -1,22 +1,21 @@
-# Merge two sorted linked lists and return it as a sorted list. 
+# Merge two sorted linked lists and return it as a sorted list.
 # The list should be made by splicing together the nodes of the first two lists.
-# 
+#
 # Example 1:
 # Input: l1 = [1,2,4], l2 = [1,3,4]
 # Output: [1,1,2,3,4,4]
-# 
+#
 # Example 2:
 # Input: l1 = [], l2 = []
 # Output: []
-# 
+#
 # Example 3:
 # Input: l1 = [], l2 = [0]
 # Output: [0]
 
-from data_structures.LinkedLists.SinglyLinkedList import SinglyLinkedList, ListNode
-from typing import List, Optional
+from typing import Optional
 from unittest import TestCase
-
+from ...data_structures.LinkedLists.SinglyLinkedList import SinglyLinkedList, ListNode
 
 class Solution:
     def __init__(self) -> None:
@@ -38,7 +37,7 @@ class Solution:
             elif l2:
                 self.__insert(l2.val)
                 l2 = l2.next
-        
+
         return self.__outputList
 
 
@@ -56,8 +55,8 @@ class TestMergingTwoSortedList(TestCase):
         self.__test([], [[], []])
         self.__test([0], [[], [0]])
         self.__test([1,1,2,3,4,4], [[1,2,4],[1,3,4]])
-    
-    
+
+
     def __test(self, result, input):
         firstList = SinglyLinkedList()
         for val in input[0]:
